d72155cf8e88e04b16cb2970e5f206f33493f23e
jhonatanstefani03 <jhonatan.silva2@alunos.sc.senac.br>
2025-06-25 20:30:07 -0300

    alteracao na agenda

--- admin/admin_controller.py
+++ admin/admin_controller.py
@@ -1,2 +1,13 @@
 def login_admin():
     print("Login funcionando!")
+    while True:
+        print("\n--- MENU DO ADMIN  ---")
+        print("[1] Cadastrar Cliente")
+        print("[2] Cadastrar Barbeiro")
+        print("[3] Remover Cliente")
+        print("[4] Remover Barbeiro")
+        print("[5] Cadastrar Horario")
+        print("")
+
+
+
